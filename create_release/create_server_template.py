41e6e406353ee8b140bc4c86d143e53376cfb01a
Pedro Ferreira <plferreira4@gmail.com>
2013-06-06 19:42:38 +0100

    - use a common create_release script for all the applications (share as much code as possible) - move the code to update the website's base.html to the release version, from the website's script to here (to be consistent as the other applications)

---
+++ create_release/create_server_template.py
@@ -0,0 +1,45 @@
+import os.path
+import re
+from bs4 import BeautifulSoup
+
+
+
+def go( resultingFolder ):
+
+        # update the base.html to point to the paint's minimized file
+
+    pathBaseHtml = os.path.join( resultingFolder, 'templates/pagina/base.html' )
+
+    with open( pathBaseHtml, 'r' ) as f:
+        baseContent = f.read()
+
+    soup = BeautifulSoup( baseContent )
+
+    scripts = soup.find_all( 'script' )
+
+    paintScripts = []
+
+        # identify the paint's scripts
+    for aScript in scripts:
+
+        src = aScript.get( 'src' )
+
+
+            # confirm that it has a 'src' attribute, and that it has the 'paint' string somewhere on its path
+        if src and re.search( 'paint', src ):
+
+            paintScripts.append( aScript )
+
+
+        # remove all but one
+    minimizedPaint = paintScripts.pop()
+
+    for aScript in paintScripts:
+        aScript.decompose()
+
+        # change the source to point to the minimized script
+    minimizedPaint['src'] = '{{ STATIC_URL }}paint/minimized.js'
+
+        # save back to base.html
+    with open( pathBaseHtml, 'w' ) as f:
+        f.write( soup.prettify() )

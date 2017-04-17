9678fe145961ebdcd3edac946455980aca2eb038
Pedro Ferreira <plferreira4@gmail.com>
2017-04-17 10:50:39 +0100

    - move the global canvas related variables to the 'Paint' namespace - initialize the canvas elements on a function in the 'Paint' namespace

--- scripts/main.ts
+++ scripts/main.ts
@@ -43,57 +43,18 @@ interface Brush
     }
 
 
-    // main canvas, will contain the drawing with all the shapes
-var MAIN_CANVAS: HTMLCanvasElement;
-var MAIN_CTX: CanvasRenderingContext2D;
-
-    // draw canvas, used to show the line being drawn only
-var DRAW_CANVAS: HTMLCanvasElement;
-var DRAW_CTX: CanvasRenderingContext2D;
-
-
 window.onload = function()
 {
-MAIN_CANVAS = <HTMLCanvasElement> document.querySelector( "#mainCanvas" );
-MAIN_CTX = MAIN_CANVAS.getContext( '2d' )!;
-
-
-DRAW_CANVAS = <HTMLCanvasElement> document.querySelector( '#drawCanvas' );
-DRAW_CTX = DRAW_CANVAS.getContext( '2d' )!;
-
-
-    // so that the cursor stays the default (instead of the text selection image)
-MAIN_CANVAS.onselectstart = function() { return false; };
-
 Color.init( SaveLoad.getRgb() );
 
 var savedCanvas = Utilities.getObject( 'saved_canvas' );
 
-if ( savedCanvas != true )
+if ( savedCanvas !== true )
     {
     savedCanvas = false;
     }
 
 Paint.init( savedCanvas );
-
-    // set the dimensions of the canvas, to fill the available space in the window
-var menuHeight = $( '#Menu' ).outerHeight();
-var windowWidth = $( window ).outerWidth();
-var windowHeight = $( window ).outerHeight();
-
-var canvasWidth = windowWidth;
-var canvasHeight = windowHeight - menuHeight;
-
-MAIN_CANVAS.width = canvasWidth;
-MAIN_CANVAS.height = canvasHeight;
-
-DRAW_CANVAS.width = canvasWidth;
-DRAW_CANVAS.height = canvasHeight;
-
-if ( savedCanvas )
-    {
-    SaveLoad.loadCanvasImage();
-    }
 };
 
 
--- scripts/paint.ts
+++ scripts/paint.ts
@@ -7,6 +7,15 @@ namespace Paint
         }
 
 
+        // main canvas, will contain the drawing with all the shapes
+    var MAIN_CANVAS: HTMLCanvasElement;
+    var MAIN_CTX: CanvasRenderingContext2D;
+
+        // draw canvas, used to show the line being drawn only
+    var DRAW_CANVAS: HTMLCanvasElement;
+    var DRAW_CTX: CanvasRenderingContext2D;
+
+
     var BRUSHES: PreviousBrushSettings[] = [
             {
                 brushClass: LineBrush,
@@ -45,6 +54,7 @@ namespace Paint
     */
     export function init( savedCanvas: boolean )
         {
+        initCanvas( savedCanvas );
         SaveLoad.loadBrushesValues( BRUSHES );
 
         var saveCanvas = document.getElementById( 'saveCanvas' )!;
@@ -87,6 +97,41 @@ namespace Paint
         document.body.onmousemove = duringDraw;
         document.body.onmouseup = endDraw;
         document.body.onkeydown = keyboardShortcuts;
+        }
+
+
+    /**
+     * Initialize the canvas elements and it their dimensions to match the available window dimension.
+     */
+    function initCanvas( savedCanvas: boolean )
+        {
+        MAIN_CANVAS = <HTMLCanvasElement> document.querySelector( "#mainCanvas" );
+        MAIN_CTX = MAIN_CANVAS.getContext( '2d' )!;
+
+        DRAW_CANVAS = <HTMLCanvasElement> document.querySelector( '#drawCanvas' );
+        DRAW_CTX = DRAW_CANVAS.getContext( '2d' )!;
+
+            // so that the cursor stays the default (instead of the text selection image)
+        MAIN_CANVAS.onselectstart = function() { return false; };
+
+            // set the dimensions of the canvas, to fill the available space in the window
+        var menuHeight = $( '#Menu' ).outerHeight();
+        var windowWidth = $( window ).outerWidth();
+        var windowHeight = $( window ).outerHeight();
+
+        var canvasWidth = windowWidth;
+        var canvasHeight = windowHeight - menuHeight;
+
+        MAIN_CANVAS.width = canvasWidth;
+        MAIN_CANVAS.height = canvasHeight;
+
+        DRAW_CANVAS.width = canvasWidth;
+        DRAW_CANVAS.height = canvasHeight;
+
+        if ( savedCanvas )
+            {
+            SaveLoad.loadCanvasImage();
+            }
         }
 
 
